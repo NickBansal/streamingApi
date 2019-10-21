0042c17e73d071b800c8f33892bf23522d63cc04
Nick Bansal <extreme_wired@msn.com>
2019-10-21 10:39:02 +0100

    set up tests for getting a users profile

--- test.js
+++ test.js
@@ -2,13 +2,18 @@ const app = require('.');
 const request = require('supertest')(app);
 // eslint-disable-next-line no-unused-vars
 const { expect } = require('chai');
+const dummyData = require('./testData');
 
 describe('/api', () => {
-  it('returns a 200 on the API route page', () => request.get('/api')
+  it('should return a 200 on the API route page', () => request.get('/api')
     .expect(200));
 
 
   describe('/streams/:user_id', () => {
-
+    it('GET returns 200 and the users profile', () => request.get(`/api/streams/${dummyData[0].userid}`)
+      .expect(200)
+      .then((res) => {
+        expect(res.body).to.be.an('object');
+      }));
   });
 });

63528c321a9a2722f29e9919d33dd38d9c7fc13f
Nick Bansal <extreme_wired@msn.com>
2019-10-21 11:15:40 +0100

    test passing for wrong url

--- index.js
+++ index.js
@@ -19,4 +19,6 @@ app.get('/api/streams/:userid', (req, res) => {
   }
 });
 
+app.use('/*', (req, res) => res.status(404).send({ msg: `${req.originalUrl} does not exist` }));
+
 module.exports = app;
